48af0beb88ef1e57672a71b5e8092fe6ec9b9277
jlim0197 <jlim0197@student.monash.edu>
2023-07-31 22:16:49 +0800

    clean up the code

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-// let express = require('express');
-// let app = express();
-// app.get('/', function (req, res) {
-//     res.send('Hello World');
-// });
-// app.get('/time', function (req, res) {
-//     let d = new Date();
-//     let msg = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-//     res.send(msg);
-// });
-// app.get('/date', function (req, res) {
-//     let d = new Date();
-//     let msg = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
-//     res.send(msg);
-// });
-// app.listen(8080);
-
-// server.js
 
 const express = require('express');
 const fs = require('fs');
@@ -32,15 +14,7 @@ async function readDataFromFile() {
     return jsonArray;
 }
 
-// API endpoint to fetch data from Table 1
-app.get('/api/table1', async (req, res) => {
-    try {
-        const table1Data = await readDataFromFile();
-        res.json(table1Data);
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching data from Table 1' });
-    }
-});
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'readcsv.html'));
