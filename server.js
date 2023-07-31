0cb22c1f3c051488a2b23d9b0087e4b492a17272
jlim0197 <jlim0197@student.monash.edu>
2023-07-31 22:18:52 +0800

    bug fix

--- server.js
+++ server.js
@@ -7,20 +7,26 @@ const csvtojson = require('csvtojson');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Function to read the CSV data and convert it to JSON
 async function readDataFromFile() {
     const dataFilePath = path.join(__dirname, 'Table_Input.csv');
     const jsonArray = await csvtojson().fromFile(dataFilePath);
     return jsonArray;
 }
 
-
+// API endpoint to fetch data from Table 1
+app.get('/api/table1', async (req, res) => {
+    try {
+        const table1Data = await readDataFromFile();
+        res.json(table1Data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from Table 1' });
+    }
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'readcsv.html'));
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
